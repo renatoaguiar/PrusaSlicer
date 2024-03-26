3b7e128690bf71949d73c2145d28a2f21862610e
Renato Aguiar <renato@renatoaguiar.net>
2024-03-25 20:26:30 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 set -e
 
+root_dir=$(realpath `dirname $0`)
+build_dir=/usr/ports/pobj/_renato/prusaslicer
+
 ulimit -d 8388608
 
-cd `dirname $0`
-mkdir -p build
-cd build
-touch .nobackup
+mkdir -p "$build_dir"
+cd "$build_dir"
 
+export CCACHE_DIR="/usr/ports/pobj/_renato/ccache"
 export CMAKE_CXX_COMPILER_LAUNCHER=ccache
 export CMAKE_C_COMPILER_LAUNCHER=ccache
 
-nice cmake .. \
+ln -fsh "$build_dir/compile_commands.json" "$root_dir/compile_commands.json"
+
+nice cmake "$root_dir" \
      -DSLIC3R_GTK=3 \
      -DSLIC3R_PCH=OFF \
      -DCMAKE_MODULE_PATH=/usr/local/lib/cmake/OpenVDB \
