30aca299c8ad84d8cecec6e84b1204e7b1ac802a
Renato Aguiar <renato@renatoaguiar.net>
2024-03-28 20:35:16 -0700

    Change build type to release

--- build.sh
+++ build.sh
@@ -20,6 +20,7 @@ nice cmake "$root_dir" \
      -DSLIC3R_PCH=OFF \
      -DCMAKE_MODULE_PATH=/usr/local/lib/cmake/OpenVDB \
      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-     -DCMAKE_BUILD_TYPE=Debug
+     -DCMAKE_BUILD_TYPE=RelWithDebInfo
+#     -DCMAKE_BUILD_TYPE=Debug
 
 nice make -j4
